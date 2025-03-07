b73548c22c7f1d2e4633b9a5e697e9dad092358a
larinpaul <www.larinpavel@mail.ru>
2025-03-07 06:43:01 +0300

    first commit

---
+++ SparkSQLBatchProcessing.scala
@@ -0,0 +1,6 @@
+// 2025 03 07 
+// https://sparkbyexamples.com/spark/spark-batch-processing-produce-consume-kafka-topic/
+
+// Spark SQL Batch Processing - Produce and Consume Apache Kafka Topic
+
+
