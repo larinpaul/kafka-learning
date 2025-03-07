6c8fe353bc0809a52c9a6c7323baebf6c3071e45
larinpaul <www.larinpavel@mail.ru>
2025-03-07 06:57:37 +0300

    // Spark SQL Batch Processing - Producing Messages to Kafka Topic.
    package com.sparkbyexamples.spark.streaming.batch
    import org.apache.spark.sql.SparkSession
    object WriteDataFrameToKafka {
    
      def main(args: Array[String]): Unit = {
    
          val spark: SparkSession = SparkSession.builder()
            .master("local[1]")
            .appName("SparkByExamples.com")
            .getOrCreate()
    
          val data = Seq (("iphone", "2007"),("iphone 3G","2008"),
            ("iphone 3GS","2009"),
            ("iphone 4","2010"),
            ("iphone 4S",2011),
            ("iphone 5","2012"),
            ("iphone 8","2014"),
            ("iphone 10","2017"))
    
          val df = spark.createDataFrame(data).toDF("key","value")
          /*
            since we are using datagrame which is laready in text,
            selectExprt is optional.
            If the bytes of the Kafka recordsrepresent UTF8 strings,
            we can simply use a cast to convert the binary data
            into the correct type.
    
            df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
          */
          df.write
            .format("kafka")
            .option("kafka.bootstrap.servers","192.169.1.100:9092")
            .option("topic","text_topic")
            .save()
      }
    }

--- SparkSQLBatchProcessing.scala
+++ SparkSQLBatchProcessing.scala
@@ -19,3 +19,44 @@
     <artifactId>spark-sql-kafka-0-10_2.11</artifactId>
     <version>2.4.0</version>
 </dependency>
+
+
+// Spark SQL Batch Processing - Producing Messages to Kafka Topic.
+
+package com.sparkbyexamples.spark.streaming.batch
+import org.apache.spark.sql.SparkSession
+object WriteDataFrameToKafka {
+
+  def main(args: Array[String]): Unit = {
+
+      val spark: SparkSession = SparkSession.builder()
+        .master("local[1]")
+        .appName("SparkByExamples.com")
+        .getOrCreate()
+
+      val data = Seq (("iphone", "2007"),("iphone 3G","2008"),
+        ("iphone 3GS","2009"),
+        ("iphone 4","2010"),
+        ("iphone 4S",2011),
+        ("iphone 5","2012"),
+        ("iphone 8","2014"),
+        ("iphone 10","2017"))
+
+      val df = spark.createDataFrame(data).toDF("key","value")
+      /*
+        since we are using datagrame which is laready in text,
+        selectExprt is optional.
+        If the bytes of the Kafka recordsrepresent UTF8 strings,
+        we can simply use a cast to convert the binary data
+        into the correct type.
+
+        df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+      */
+      df.write
+        .format("kafka")
+        .option("kafka.bootstrap.servers","192.169.1.100:9092")
+        .option("topic","text_topic")
+        .save()
+  }
+}
+
