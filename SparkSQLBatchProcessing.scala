d3181765ec6badb1d36105c9af560d9708e5e3c7
larinpaul <www.larinpavel@mail.ru>
2025-03-07 06:49:41 +0300

    // Maven Dependency for Apache Kafka: <dependency>     <groupId>org.apache.spark</groupId>     <artifactId>spark-sql-kafka-0-10_2.11</artifactId>     <version>2.4.0</version> </dependency>

--- SparkSQLBatchProcessing.scala
+++ SparkSQLBatchProcessing.scala
@@ -4,3 +4,18 @@
 // Spark SQL Batch Processing - Produce and Consume Apache Kafka Topic
 
 
+// This article describes Spark SQL Batch Processing using Apache Kafka Data Source on DataFrame.
+// Unlike Spark structure stream processing, we may need to process batch jobs that consume the messages
+// from Apache Kafka topic and produces messages to Apache Kafka topic in batch mode.
+// To do this we should use read instead of readStream similarly write instead of writeStream on DataFrame.
+
+// Spark SQL Batch Processing - Producing Messages to Kafka Topic.
+// Spark SQL Batch Processing - Consuming Messages from Kafka Topic. 
+
+
+// Maven Dependency for Apache Kafka:
+<dependency>
+    <groupId>org.apache.spark</groupId>
+    <artifactId>spark-sql-kafka-0-10_2.11</artifactId>
+    <version>2.4.0</version>
+</dependency>
