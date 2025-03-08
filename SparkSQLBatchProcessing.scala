72ea089e2e16108eca2859260b46bf7ace2e0123
larinpaul <www.larinpavel@mail.ru>
2025-03-08 09:23:20 +0300

    package com.sparkbyexamples.spark.streaming.batch import org.apache.spark.sql.SparkSession object ReadDataFromKafka {
      def main(args: Array[String]): Unit = {
    
        val spark: SparkSession = SparkSession.builder()
          .master("local[1]")
          .appName("https://sparkbyexamples.com")
          .getOrCreate()
    
        val df = spark
          .read
          .format("kafka")
          .option("kafka.bootstrap.servers", "192.168.1.100:9092")
          .option("subscribe", "text_topic")
          .load()
    
        df.printSchema()
    
        val df2 = df.selectExpr("CAST(key AS STRING)",
                  "CAST(value AS STRING)", "topic")
        df2.show(false)
      }
    }

--- SparkSQLBatchProcessing.scala
+++ SparkSQLBatchProcessing.scala
@@ -60,3 +60,31 @@ object WriteDataFrameToKafka {
   }
 }
 
+
+// Spark SQL Batch Processing - Consuming Messages from Kafka Topic
+
+package com.sparkbyexamples.spark.streaming.batch
+import org.apache.spark.sql.SparkSession
+object ReadDataFromKafka {
+  
+  def main(args: Array[String]): Unit = {
+
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[1]")
+      .appName("https://sparkbyexamples.com")
+      .getOrCreate()
+
+    val df = spark
+      .read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "192.168.1.100:9092")
+      .option("subscribe", "text_topic")
+      .load()
+
+    df.printSchema()
+
+    val df2 = df.selectExpr("CAST(key AS STRING)",
+              "CAST(value AS STRING)", "topic")
+    df2.show(false)
+  }
+}
