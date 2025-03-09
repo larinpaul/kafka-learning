ccbd17cd516da22b3ec6483862fd9b8f2a4fd6f1
larinpaul <www.larinpavel@mail.ru>
2025-03-09 08:08:30 +0300

    // 2025 03 09
    // https://www.baeldung.com/apache-kafka
    
    // Introduction to Apache Kafka
    
    // 1. Overview
    // In this tutorial, we'll learn the basics of Kafka - the use caes and core concepts anyone should know.
    // We can then find and understand more detailed articles about Kafka // https://www.baeldung.com/tag/kafka
    
    // 2. What Is Kafka?
    
    // https://kafka.apache.org/
    // Kafka is an open-source stream processing platform developed by the Apache Software Foundation.
    // We can use it as a messaging system to decouple message producers and consumers,
    // but in comparison to "classical" messaging systems like ActiveMQ,
    // it is designed to handle real-time data streams and provides
    // a distributed, fault-tolerant, and highly scalable architecture for processing and storing data.
    
    // Therefore, we can use it in various use cases:
    // * Real-time data processing and analytics
    // * Log and event data aggregation
    // * Monitoring and metrics collection
    // * Clickstream data analytics
    // * Fraud detection
    // * Stream processing in big data pipelines

---
+++ 02_IntroductionToApacheKafka.java
@@ -0,0 +1,30 @@
+// 2025 03 09
+
+// https://www.baeldung.com/apache-kafka
+
+// Introduction to Apache Kafka
+
+
+// 1. Overview
+// In this tutorial, we'll learn the basics of Kafka - the use caes and core concepts anyone should know.
+// We can then find and understand more detailed articles about Kafka // https://www.baeldung.com/tag/kafka
+
+
+// 2. What Is Kafka?
+
+// https://kafka.apache.org/
+// Kafka is an open-source stream processing platform developed by the Apache Software Foundation.
+// We can use it as a messaging system to decouple message producers and consumers,
+// but in comparison to "classical" messaging systems like ActiveMQ,
+// it is designed to handle real-time data streams and provides
+// a distributed, fault-tolerant, and highly scalable architecture for processing and storing data.
+
+// Therefore, we can use it in various use cases:
+// * Real-time data processing and analytics
+// * Log and event data aggregation
+// * Monitoring and metrics collection
+// * Clickstream data analytics
+// * Fraud detection
+// * Stream processing in big data pipelines
+
+
