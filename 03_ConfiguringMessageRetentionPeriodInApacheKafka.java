e0ccd9b157551636beab9ea059046c9fe21315fa
larinpaul <www.larinpavel@mail.ru>
2025-03-16 09:29:41 +0300

    $ grep -o 'log.retention.[hms].*\=' config/server.properties log.retention.hours=168 # seven days
    log.retention.minutes=10 # 10 minutes

--- 03_ConfiguringMessageRetentionPeriodInApacheKafka.java
+++ 03_ConfiguringMessageRetentionPeriodInApacheKafka.java
@@ -34,6 +34,15 @@
 // First, let's inspect the default value for retention by executing the grep command // https://www.baeldung.com/linux/grep-sed-awk-differences#grep // The grep command searches for lines matching a regex expression and prints those matching lines to the standard output. It is useful when we need a quick way to find out whether a particular pattern exists or not in the given input. // grep [OPTION] PATTERN [FILE...]
 // from the Apache Kafka directory: // https://kafka.apache.org/documentation/#quickstart
 
+$ grep -o 'log.retention.[hms].*\=' config/server.properties
+log.retention.hours=168
+
+// We can notice here that the default retention time is seven dayas.
+
+// To retain messages only for ten minutes, we can set the value of the log.retention.minutes property
+// in the config/server.properties:
+log.retention.minutes=10
+
 
 // A topic resides within the broker and usually has different partitions
 
