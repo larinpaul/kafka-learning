d61f29781049444fe70d6993defad8e5e4a042f1
larinpaul <www.larinpavel@mail.ru>
2025-03-22 03:43:45 +0300

    // https://www.baeldung.com/spring-kafka
    // 1. Overview
    
    // Apache Kafka is a distributed and fault-tolerant stream processing system.
    
    // In this tutorial, we'll cover Spring support for Kafka and its abstraction level
    // over native Kafka Java client APIs.
    
    // Spring Kafka brings the simple and typical Spring template programming model
    // with a KafkaTemplate and Message-driven POJOs via @KafkaListener annotation.

---
+++ 04_intro_to_apache_kafka_with_spring.java
@@ -0,0 +1,14 @@
+// https://www.baeldung.com/spring-kafka
+
+// 1. Overview
+
+// Apache Kafka is a distributed and fault-tolerant stream processing system.
+
+// In this tutorial, we'll cover Spring support for Kafka and its abstraction level
+// over native Kafka Java client APIs.
+
+// Spring Kafka brings the simple and typical Spring template programming model
+// with a KafkaTemplate and Message-driven POJOs via @KafkaListener annotation.
+
+
+
