25d4ebcbfa8c49a07d74ce36af650d9489bb5034
larinpaul <www.larinpavel@mail.ru>
2025-04-14 09:37:25 +0300

    // 2025 04 14
    // https://www.baeldung.com/logstash-kafka-differences
    
    // Last updated: October 22, 2024
    
    // 1. Overview
    
    // Logstash and Kafka are two powerful tools for managing real-time data streams.
    // While Kafka excels as a distributed event streaming platform,
    // Logstash is a data processing pipeline for ingesting, filtering, and forwarding data to various outputs.
    
    // In this tutorial, we'll examine the difference between Kafka and Logdash in more detail
    // and provide examples of their usage.

---
+++ LogstashVsKafka.java
@@ -0,0 +1,19 @@
+// 2025 04 14
+
+// https://www.baeldung.com/logstash-kafka-differences
+
+// Last updated: October 22, 2024 
+
+
+// 1. Overview
+
+// Logstash and Kafka are two powerful tools for managing real-time data streams.
+// While Kafka excels as a distributed event streaming platform,
+// Logstash is a data processing pipeline for ingesting, filtering, and forwarding data to various outputs.
+
+// In this tutorial, we'll examine the difference between Kafka and Logdash in more detail 
+// and provide examples of their usage.
+
+
+
+
