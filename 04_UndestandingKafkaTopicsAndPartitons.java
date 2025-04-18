95d2250a3a5ba80083d303018c70566eacf40c14
larinpaul <www.larinpavel@mail.ru>
2025-04-18 09:09:40 +0300

    // A topic is a storage mechanism for a sequence of events. // Eventually, topics are durable log files that keep events in the same order as ther occur in time.

--- 04_UndestandingKafkaTopicsAndPartitons.java
+++ 04_UndestandingKafkaTopicsAndPartitons.java
@@ -11,6 +11,10 @@
 // In this tutorial, we'll explore Kafka topics and partitions and how they relate to each other // https://www.baeldung.com/spring-kafka
 
 
+// 2. What Is a Kafka Topics
+
+// A topic is a storage mechanism for a sequence of events.
+// Eventually, topics are durable log files that keep events in the same order as ther occur in time.
 
 
 
