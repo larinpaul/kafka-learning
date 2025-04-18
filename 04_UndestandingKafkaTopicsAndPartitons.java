0aa7f6f3c0e396fc4649617bf7ebe8c63c970d2c
larinpaul <www.larinpavel@mail.ru>
2025-04-18 09:08:42 +0300

    // 2025 04 18
    // https://www.baeldung.com/kafka-topics-partitions
    // Understanding Kafka Topics and Partitons
    
    // Last updated: May 11, 2024
    
    // 1. Introduction
    // In this tutorial, we'll explore Kafka topics and partitions and how they relate to each other // https://www.baeldung.com/spring-kafka

---
+++ 04_UndestandingKafkaTopicsAndPartitons.java
@@ -0,0 +1,19 @@
+// 2025 04 18
+
+
+// https://www.baeldung.com/kafka-topics-partitions
+// Understanding Kafka Topics and Partitons
+
+// Last updated: May 11, 2024
+
+
+// 1. Introduction
+// In this tutorial, we'll explore Kafka topics and partitions and how they relate to each other // https://www.baeldung.com/spring-kafka
+
+
+
+
+
+
+
+
