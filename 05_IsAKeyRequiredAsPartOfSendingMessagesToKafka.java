f88897005d5dc39a1db26934ec56d1067e4a6554
larinpaul <www.larinpavel@mail.ru>
2025-04-27 12:14:06 +0300

    public class 05_IsAKeyRequiredAsPartOfSendingMessagesToKafka {
        // 2025 04 27
    
        // https://www.baeldung.com/java-kafka-message-key
    
        // Last updated: January 8, 2024
    
        // Written by: Avin Buricha
        // Reviewed by: Saajan Nagendra
    
    }

---
+++ 05_IsAKeyRequiredAsPartOfSendingMessagesToKafka.java
@@ -0,0 +1,16 @@
+public class 05_IsAKeyRequiredAsPartOfSendingMessagesToKafka {
+    
+    // 2025 04 27
+
+    // https://www.baeldung.com/java-kafka-message-key
+
+    // Last updated: January 8, 2024
+
+    // Written by: Avin Buricha
+    // Reviewed by: Saajan Nagendra
+
+    
+
+
+
+}
