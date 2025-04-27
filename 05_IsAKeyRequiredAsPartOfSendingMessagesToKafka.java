efbd7c29fec14254b555792af0d18ac7b10e67b7
larinpaul <www.larinpavel@mail.ru>
2025-04-27 12:16:03 +0300

    // 1. Intorudction
        // Apache Kafka is an open-source and distributed stream processing system // https://www.baeldung.com/spring-kafka
        // that is fuault-tolerant and provides high troughput.
        // Kafka is basically a messaging system that implements a publisher-subscriber model.
        // The messaging, storage, and stream processing capabilities of Kafka allow us to store
        // and analyze realt-time data streams at scale.
    
        // In this tutorial, we'll first look at the significaance of a key in a Kafka message.
        // We'll then learn how we can publish messages with a key to a Kafka topic.

--- 05_IsAKeyRequiredAsPartOfSendingMessagesToKafka.java
+++ 05_IsAKeyRequiredAsPartOfSendingMessagesToKafka.java
@@ -9,7 +9,20 @@ public class 05_IsAKeyRequiredAsPartOfSendingMessagesToKafka {
     // Written by: Avin Buricha
     // Reviewed by: Saajan Nagendra
 
+
+    // 1. Intorudction
+
+    // Apache Kafka is an open-source and distributed stream processing system // https://www.baeldung.com/spring-kafka
+    // that is fuault-tolerant and provides high troughput.
+    // Kafka is basically a messaging system that implements a publisher-subscriber model.
+    // The messaging, storage, and stream processing capabilities of Kafka allow us to store
+    // and analyze realt-time data streams at scale.
+
+    // In this tutorial, we'll first look at the significaance of a key in a Kafka message.
+    // We'll then learn how we can publish messages with a key to a Kafka topic.
+
     
+
 
 
 
