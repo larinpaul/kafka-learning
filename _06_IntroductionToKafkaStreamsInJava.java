556549844c460c90ffa848f3ad906f13805d1c1b
larinpaul <www.larinpavel@mail.ru>
2025-04-30 07:03:56 +0300

    // 1. Overview
        // In this article, we'll be looking at the KafkaStreams library. // https://kafka.apache.org/documentation/streams/
    
        // KafkaStreams is engineerd by the creators of Apache Kafka.
        // The primary goal of this piece of software is to allow programmers
        // to create efficient, real-time streaming applications that could work as Microservices.
    
        // KafkaStreams enables us to consume from Kafka topics,
        // analyze or transform data, and potentially, send it to another Kafka topic.
    
        // To demonstrate KafkaStreams, we'll create a simple application that reads sentences
        // from a topic, counts occurrences of words and prints the count per word.
    
        // Important to note is that the KafkaStreams library isn't reactive
        // and has no support for async operations and backpressure handling.

--- _06_IntroductionToKafkaStreamsInJava.java
+++ _06_IntroductionToKafkaStreamsInJava.java
@@ -4,7 +4,29 @@ public class _06_IntroductionToKafkaStreamsInJava {
 
     // Introduction to KafkaStreams in Java
 
+    // Last updated: January 8, 2024
+    // Written by: baeldung
+
+  
+    // 1. Overview
+
+    // In this article, we'll be looking at the KafkaStreams library. // https://kafka.apache.org/documentation/streams/
+
+    // KafkaStreams is engineerd by the creators of Apache Kafka.
+    // The primary goal of this piece of software is to allow programmers
+    // to create efficient, real-time streaming applications that could work as Microservices.
+
+    // KafkaStreams enables us to consume from Kafka topics,
+    // analyze or transform data, and potentially, send it to another Kafka topic.
+
+    // To demonstrate KafkaStreams, we'll create a simple application that reads sentences
+    // from a topic, counts occurrences of words and prints the count per word.
+
+    // Important to note is that the KafkaStreams library isn't reactive
+    // and has no support for async operations and backpressure handling.
+
     
+
 
 
 }
