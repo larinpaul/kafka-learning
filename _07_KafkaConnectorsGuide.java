25c6f5b1cfbfe20b43a0b8c58c3af1dc5419c2a5
larinpaul <www.larinpavel@mail.ru>
2025-05-03 10:40:03 +0300

    // 2025 05 03
    // 1. Overview
    
    // Apache Kafka® is a distributed streaming platform.
    // In a previous tutorial, we discussed
    // how to implement Kafka consumers and producers using Spring. // https://www.baeldung.com/spring-kafka
    
    // In this tutorial, we'll learn how to use Kafka Connectors.
    
    // We'll have a look at:
    // * Different types of Kafka Connectors
    // * Featuresand modes of Kafka Connect
    // * Connectors configuration using property files as well as the REST API.

---
+++ _07_KafkaConnectorsGuide.java
@@ -0,0 +1,17 @@
+// 2025 05 03
+
+// 1. Overview
+
+// Apache Kafka® is a distributed streaming platform.
+// In a previous tutorial, we discussed 
+// how to implement Kafka consumers and producers using Spring. // https://www.baeldung.com/spring-kafka
+
+// In this tutorial, we'll learn how to use Kafka Connectors.
+
+// We'll have a look at:
+// * Different types of Kafka Connectors
+// * Featuresand modes of Kafka Connect
+// * Connectors configuration using property files as well as the REST API.
+
+
+
