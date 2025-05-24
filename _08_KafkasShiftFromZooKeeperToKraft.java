19a0aaed8364648d5b4cc61d2b54b01a7681292f
larinpaul <www.larinpavel@mail.ru>
2025-05-24 13:20:56 +0300

    // 1. Introduction
    // Kafka, in its architecture, has recently shifted from ZooKeeper to a quorum-based
    // controller that uses a new consensus protocol called Kafka Raft,
    // shortened as Kraft (pronouncedd as "craft").
    
    // In this tutorial, we'll examine the reson why Kafka has taken this decision
    // and how the change simplifies its architecture
    // and makes it more powerful to use.

--- _08_KafkasShiftFromZooKeeperToKraft.java
+++ _08_KafkasShiftFromZooKeeperToKraft.java
@@ -4,6 +4,15 @@
 // https://www.baeldung.com/kafka-shift-from-zookeeper-to-kraft
 // Last updated: Feruary 13, 2024
 
+// 1. Introduction 
+
+// Kafka, in its architecture, has recently shifted from ZooKeeper to a quorum-based
+// controller that uses a new consensus protocol called Kafka Raft,
+// shortened as Kraft (pronouncedd as "craft").
+
+// In this tutorial, we'll examine the reson why Kafka has taken this decision
+// and how the change simplifies its architecture
+// and makes it more powerful to use.
 
 
 
