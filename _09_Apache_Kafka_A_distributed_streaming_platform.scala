a353f204876eb756a2edd36dc4c3428948fdda0d
larinpaul <www.larinpavel@mail.ru>
2025-05-26 06:47:07 +0300

    // 2025 05 26
    // https://sparkbyexamples.com/kafka/apache-kafka-consumer-producer-in-scala/
    
    // APACHE kafka A distributed streaming platform
    
    // In this Scala & Kafa tutorial, you will learn
    // how to write Kafka messages to Kafka topic (producer)
    // and read messages from topic (consumer) using Scala example;
    // producer sends messages to Kafka topics in the form of records,
    // a record is a key-value pair along with topic name
    // and consumer receives a message from a topic.

---
+++ _09_Apache_Kafka_A_distributed_streaming_platform.scala
@@ -0,0 +1,23 @@
+// 2025 05 26
+
+// https://sparkbyexamples.com/kafka/apache-kafka-consumer-producer-in-scala/
+
+// APACHE kafka A distributed streaming platform
+
+
+// In this Scala & Kafa tutorial, you will learn
+// how to write Kafka messages to Kafka topic (producer)
+// and read messages from topic (consumer) using Scala example;
+// producer sends messages to Kafka topics in the form of records,
+// a record is a key-value pair along with topic name
+// and consumer receives a message from a topic.
+
+
+
+
+
+
+
+
+
+
