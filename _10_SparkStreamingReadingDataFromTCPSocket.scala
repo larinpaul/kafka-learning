572d303bcd3420c7caeb604807659e40094e8db8
larinpaul <www.larinpavel@mail.ru>
2025-05-29 07:02:41 +0300

    // 2025 05 29
    // https://sparkbyexamples.com/spark/spark-streaming-from-tcp-socket/
    
    // Naveen Nelamali // Apache Spark // March 27, 2024
    
    // Data receives to TCP socket from sources => [TCP socket] => Data streams from TCP socket => Spark Streaming => Batches of input Data => Spark Engine => Batches of processed Data to S3, HDFS, HBase etc. => S3, HDFS, HBase, Kafka, Others

---
+++ _10_SparkStreamingReadingDataFromTCPSocket.scala
@@ -0,0 +1,12 @@
+// 2025 05 29
+
+// https://sparkbyexamples.com/spark/spark-streaming-from-tcp-socket/
+
+// Naveen Nelamali // Apache Spark // March 27, 2024
+
+
+// Data receives to TCP socket from sources => [TCP socket] => Data streams from TCP socket => Spark Streaming => Batches of input Data => Spark Engine => Batches of processed Data to S3, HDFS, HBase etc. => S3, HDFS, HBase, Kafka, Others
+
+
+
+
