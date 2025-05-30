9b9fe1dc8f481ca6db16b4e5d9bd758a8881563e
larinpaul <www.larinpavel@mail.ru>
2025-05-30 07:26:29 +0300

    // Using Spark streaming we will see a working example of how to read data from TCP Socket, // process it and write output to console. // Spark uses readStream() to read and writeStream() to write streaming DataFrame or Dataset. // The below-explained example does the word count on streaming data and outputs the result to console.

--- _10_SparkStreamingReadingDataFromTCPSocket.scala
+++ _10_SparkStreamingReadingDataFromTCPSocket.scala
@@ -7,6 +7,9 @@
 
 // Data receives to TCP socket from sources => [TCP socket] => Data streams from TCP socket => Spark Streaming => Batches of input Data => Spark Engine => Batches of processed Data to S3, HDFS, HBase etc. => S3, HDFS, HBase, Kafka, Others
 
-
+// Using Spark streaming we will see a working example of how to read data from TCP Socket,
+// process it and write output to console.
+// Spark uses readStream() to read and writeStream() to write streaming DataFrame or Dataset.
+// The below-explained example does the word count on streaming data and outputs the result to console.
 
 
