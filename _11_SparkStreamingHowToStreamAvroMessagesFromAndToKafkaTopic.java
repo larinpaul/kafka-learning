a19d779e5974a850f0c00d221685c10e1deab716
larinpaul <www.larinpavel@mail.ru>
2025-06-04 07:04:01 +0300

    // 2025 06 04
    // https://sparkbyexamples.com/spark/spark-streaming-consume-and-produce-kafka-messages-in-avro-format/
    
    // Spark Streaming - How to Stream Avro messages from/to Kafka topic.
    
    // This article describes Spark Structured Streaming from Kafka in Avro file format
    //

---
+++ _11_SparkStreamingHowToStreamAvroMessagesFromAndToKafkaTopic.java
@@ -0,0 +1,12 @@
+// 2025 06 04
+
+// https://sparkbyexamples.com/spark/spark-streaming-consume-and-produce-kafka-messages-in-avro-format/
+
+// Spark Streaming - How to Stream Avro messages from/to Kafka topic.
+
+// This article describes Spark Structured Streaming from Kafka in Avro file format
+// 
+
+
+
+
