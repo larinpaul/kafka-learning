e7b21f0da6ab64d3c19fd9252545ce1f6ef15ab8
larinpaul <www.larinpavel@mail.ru>
2025-06-05 07:03:46 +0300

    // Before deep-diving into this further, let's understand a few points // regarding Spark Streaming, Kafka and Avro.
    // Spark Streaming is a scalable, high-throughput, fault-tolerant streaming processing system
    // that supports both batch and streaming workloads.
    // It is an extension of the core Spark API to process real-time data
    // from sources like Kafka, Flume, and Amazon Kinesis to name it a few.
    // This processed data can be pushed to databases, Kafka, live dashboards e.t.c

--- _11_SparkStreamingHowToStreamAvroMessagesFromAndToKafkaTopic.java
+++ _11_SparkStreamingHowToStreamAvroMessagesFromAndToKafkaTopic.java
@@ -17,3 +17,16 @@
 // ** Running Producer with Example
 // ** Running Consumer with Example
 
+// Before deep-diving into this further, let's understand a few points 
+// regarding Spark Streaming, Kafka and Avro.
+
+// Spark Streaming is a scalable, high-throughput, fault-tolerant streaming processing system
+// that supports both batch and streaming workloads.
+// It is an extension of the core Spark API to process real-time data 
+// from sources like Kafka, Flume, and Amazon Kinesis to name it a few.
+// This processed data can be pushed to databases, Kafka, live dashboards e.t.c
+
+
+
+
+
